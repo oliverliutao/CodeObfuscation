a97e526dd4e1965995103ae6345be65fb7f79be6
oliver <309242320@qq.com>
2017-04-19 15:05:25 +0800

    first

---
+++ TestConfusion/TestConfusion/codeObfuscation.h
@@ -0,0 +1,6 @@
+#ifndef Demo_codeObfuscation_h
+#define Demo_codeObfuscation_h
+//confuse string at Mon Feb 20 18:13:23 CST 2017
+#define testStr cIXjHSrXxsTRIgMK
+#define syn_loginWithName NMTBbBomqWkgxQtY
+#endif
